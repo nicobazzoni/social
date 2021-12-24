2bbf591b9b309d7640c174177ad57b8f4a79cc8a
Nicholas Bazzoni <nicholasbazzoni@Nicholass-iMac.fios-router.home>
2021-12-24 00:23:59 -0500

    new vercel back

---
+++ schemas/user.js
@@ -0,0 +1,17 @@
+export default{
+    name: 'user',
+    title: 'User',
+    type: 'document',
+    fields: [
+        {
+    name:'userName',
+    titlel: 'UserName',
+    type: 'string'
+},
+{
+    name: 'image',
+    title: 'Image',
+    type: 'string'
+}
+    ]
+}
